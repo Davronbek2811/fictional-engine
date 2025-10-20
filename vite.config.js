eaaa743630070927cce098331724ddad45298886
Dvronjon <davronjon176@gamil.com>
2025-10-20 15:28:37 +0500

    change logo

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/fictional-engine/'
-})
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
+  base: '/fictional-engine/',  // Repo nomini yozing
+});
